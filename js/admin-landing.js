576c24b992b82d338094dc43591cf806ea26ff27
lyndanegron <lyndanegron@gmail.com>
2015-05-06 23:34:30 -0400

    pictures work

--- js/admin-landing.js
+++ js/admin-landing.js
@@ -14,14 +14,15 @@ function updateFromStorage() {
     	retrieved = JSON.parse(localStorage[i]);
     	if (i == "townhall"){
     		deleteTownHall(i);
-    		addTownHall(retrieved['name'], retrieved['desc']);
+    		addTownHall(retrieved['name'], retrieved['desc'], retrieved['img']);
     	};
   };
 };
 
 function addTownHall(name, desc){
-  var html = '<div class="row"><div class="col-sm-6 col-md-4"><div class="thumbnail"><img src="img/housing.jpg" height="150" width="150"><div class="caption"><h3>'+name+'</h3><p>'+desc+'</p><p><a href="admin-townhall-stats.html" class="btn btn-primary" role="button">View</a> </p></div></div></div></div>'
-	townhalls.append(html);
+  var html = '<div class="row"><div class="col-sm-6 col-md-4"><div class="thumbnail"><img src="" id="pic" height="150" width="150"><div class="caption"><h3>'+name+'</h3><p>'+desc+'</p><p><a href="admin-townhall-stats.html" class="btn btn-primary" role="button">View</a> </p></div></div></div></div>';
+  townhalls.append(html);
+  getImage();
 };
 
 function deleteTownHall(question){
@@ -33,6 +34,24 @@ function getName(){
   return name;
 };
 
+function getImage(){
+   var preview = document.getElementById('pic'); //selects the query named img
+    var file    = document.querySelector('input[type=file]').files[0]; //sames as here
+    var reader  = new FileReader();
+
+       reader.onloadend = function () {
+           preview.src = reader.result;
+       }
+
+       if (file) {
+           reader.readAsDataURL(file); //reads the data as a URL
+
+       } else {
+           preview.src = "";
+       }
+};
+
+
 function getDescription(){
   var desc = document.getElementById("desc").value;
   return desc;
@@ -40,9 +59,9 @@ function getDescription(){
 
 function makeHall(){
   var name = getName();
-  var desc = getDescription();
-    
+  var desc = getDescription();    
   addTownHall(name, desc);
+
   obj = {'name': name,'desc': desc};
   localStorage.setItem("townhall", JSON.stringify(obj));
 };
